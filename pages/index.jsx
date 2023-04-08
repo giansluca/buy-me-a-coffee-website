63194fa166de6668c7e8f819e2f17568b0e3f624
gians <giansluca@gmail.com>
2023-04-08 17:07:44 +0200

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import { ethers, BrowserProvider, Contract } from "ethers";
 import styles from "../styles/Home.module.css";
-import abi from "../utils/BuyMeACoffee.json";
+import abi from "../utils/abi/BuyMeACoffee.json";
 
 export default function Home() {
     // Contract Address & ABI
